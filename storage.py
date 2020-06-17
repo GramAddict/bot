09fc3b9e14e87c4e4a15279228e3845f910e3667
Alexander Mishchenko <admishchenko@yandex-team.ru>
2020-06-17 12:07:50 +0300

    Change storage format to JSON. Please be careful: after update to this version interacted_users.txt file will be ignored!

--- storage.py
+++ storage.py
@@ -1,21 +1,38 @@
-FILENAME_INTERACTED_USERS = "interacted_users.txt"
+import json
+import os
+from datetime import datetime
+from enum import Enum, unique
+
+FILENAME_INTERACTED_USERS = "interacted_users.json"
+USER_LAST_INTERACTION = "last_interaction"
+USER_FOLLOWING_STATUS = "following_status"
 
 
 class Storage:
-    interacted_users_list = []
+    interacted_users = {}
 
     def __init__(self):
-        file_interacted_users = open(FILENAME_INTERACTED_USERS, "a+")
-        file_interacted_users.seek(0)
-        for line in file_interacted_users.readlines():
-            self.interacted_users_list.append(line.strip())
-        file_interacted_users.close()
+        if os.path.exists(FILENAME_INTERACTED_USERS):
+            with open(FILENAME_INTERACTED_USERS) as json_file:
+                self.interacted_users = json.load(json_file)
 
     def check_user_was_interacted(self, username):
-        return username in self.interacted_users_list
+        return not self.interacted_users.get(username) is None
 
     def add_interacted_user(self, username):
-        self.interacted_users_list.append(username)
-        file_interacted_users = open(FILENAME_INTERACTED_USERS, "a")
-        file_interacted_users.write(username + '\n')
-        file_interacted_users.close()
+        user = self.interacted_users.get(username, {})
+        user[USER_LAST_INTERACTION] = str(datetime.now())
+        user[USER_FOLLOWING_STATUS] = FollowingStatus.NONE.name.lower()
+        self.interacted_users[username] = user
+        self._update_file()
+
+    def _update_file(self):
+        with open(FILENAME_INTERACTED_USERS, 'w') as outfile:
+            json.dump(self.interacted_users, outfile)
+
+
+@unique
+class FollowingStatus(Enum):
+    NONE = 0
+    FOLLOWED = 1
+    UNFOLLOWED = 2
