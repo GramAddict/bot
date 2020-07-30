bffb0e1f12edceb556f32b7325b901186c4ad0c5
DNUSH <dnushd@gmail.com>
2020-07-30 09:55:41 -0400

    New --min_following
    New --min_following

--- src/action_unfollow.py
+++ src/action_unfollow.py
@@ -1,11 +1,13 @@
 from src.storage import FollowingStatus
 from src.utils import *
 
-
-def unfollow(device, count, on_unfollow, storage, only_non_followers, my_username):
+#
+## added min_following and following_count to unfollow function
+def unfollow(device, count, on_unfollow, storage, only_non_followers, my_username, following_count, min_following):
     _open_my_followings(device)
     _sort_followings_by_date(device)
-    _iterate_over_followings(device, count, on_unfollow, storage, only_non_followers, my_username)
+    ## added min_following and following_count to _iterate_over_followings function
+    _iterate_over_followings(device, count, on_unfollow, storage, only_non_followers, my_username, following_count, min_following)
 
 
 def _open_my_followings(device):
@@ -28,8 +30,23 @@ def _sort_followings_by_date(device):
     sort_options_recycler_view = device(resourceId='com.instagram.android:id/follow_list_sorting_options_recycler_view')
     sort_options_recycler_view.child(index=2).click.wait()
 
+## added min_following and following_count to _iterate_over_followings function
+def _iterate_over_followings(device, count, on_unfollow, storage, only_non_followers, my_username, following_count, min_following):
+    
+    following_deduction = 0
+    # If following is = 0 then validate limit
+    if min_following > 0:
+        if following_count <= min_following:
+            print("\033[91mYou currently have a total of ["+ str(following_count) +"] users follorwing, the minimum assigned is ["+ str(min_following) +"], ending session....\033[37m")
+            return
+        # Calculate how many following we can remove form user
+        following_deduction = (following_count - min_following)
+        following_total = (following_count - following_deduction)
+        print("\033[96mUser Currently has ["+ str(following_count) +"] and would stop unfollowing at ["+ str(following_deduction) +"] unfollows, total user followings after job ["+ str(following_total) +"]\033[37m")
+    else:
+        print("\033[96mUser Currently has ["+ str(following_count) +"] and it would not stop unfollowing!\033[37m")
 
-def _iterate_over_followings(device, count, on_unfollow, storage, only_non_followers, my_username):
+
     unfollowed_count = 0
     while True:
         print("Iterate over visible followings")
@@ -37,6 +54,12 @@ def _iterate_over_followings(device, count, on_unfollow, storage, only_non_follo
 
         for item in device(resourceId='com.instagram.android:id/follow_list_container',
                            className='android.widget.LinearLayout'):
+
+            # if we've reach the minimum amount of users following and min_following is more than 0, print message and exit
+            if unfollowed_count >= following_deduction and min_following > 0:
+                print("\033[96mUnfollow stopped, you have reach the minimum amount of following you could have\033[37m")
+                return
+
             user_info_view = item.child(index=1)
             user_name_view = user_info_view.child(index=0).child()
             if not user_name_view.exists:
