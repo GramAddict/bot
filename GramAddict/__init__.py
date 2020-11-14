8d8b58a0c045f8f9710a4d9908ceac317ac09116
Philip Ulrich <philip@ulrich.life>
2020-11-14 00:40:55 -0600

    adding soft ban info for missing profile info (#46)
    * adding soft ban info for missing profile info

--- GramAddict/__init__.py
+++ GramAddict/__init__.py
@@ -165,7 +165,10 @@ def run():
             or not session_state.my_followers_count
             or not session_state.my_following_count
         ):
-            logger.critical("Could not get profile info")
+            logger.critical(
+                "Could not get one of the following from your profile: username, # of followers, # of followings. This is typically due to a soft ban. Review the crash screenshot to see if this is the case."
+            )
+            save_crash(device)
             exit(1)
 
         username = session_state.my_username
