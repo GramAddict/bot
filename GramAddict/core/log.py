d99bb33f4564a90be52b8346f7e8d2e526acf093
Philip Ulrich <philip@ulrich.life>
2020-11-18 20:06:09 -0600

    Hotfix logging (#75)
    🐈 adding file logging and fixing crash logging
    🐛 adding try/except for version check
    
    Co-authored-by: Arthur Silva <arthursilva.ee@gmail.com>

--- GramAddict/core/log.py
+++ GramAddict/core/log.py
@@ -1,9 +1,10 @@
 import logging
-from io import StringIO
-from logging import LogRecord
-
+import os
 from colorama import Fore, Style
 from colorama import init as init_colorama
+from logging import LogRecord
+from logging.handlers import RotatingFileHandler
+from uuid import uuid4
 
 COLORS = {
     "DEBUG": Style.DIM,
@@ -28,45 +29,104 @@ class ColoredFormatter(logging.Formatter):
         return msg
 
 
-def configure_logger():
-    init_colorama()
-    logger = logging.getLogger()  # root logger
-    logger.setLevel(logging.INFO)
+class LoggerFilterGramAddictOnly(logging.Filter):
+    def filter(self, record: LogRecord):
+        return record.name.startswith("GramAddict")
 
-    # Formatters
-    datefmt = r"[%m/%d %H:%M:%S]"
-    console_fmt = "%(asctime)s %(levelname)8s | %(message)s"
-    console_formatter = ColoredFormatter(fmt=console_fmt, datefmt=datefmt)
-    crash_report_fmt = (
-        "%(asctime)s %(levelname)8s | %(message)s (%(filename)s:%(lineno)d)"
+
+def create_log_file_handler(filename):
+    file_handler = RotatingFileHandler(
+        filename,
+        mode="a",
+        backupCount=10,
+        maxBytes=1000000,
+        encoding="utf-8",
     )
-    crash_report_formatter = logging.Formatter(fmt=crash_report_fmt, datefmt=datefmt)
 
-    # Filters
-    class FilterGramAddictOnly(logging.Filter):
-        def filter(self, record: LogRecord):
-            return record.name.startswith("GramAddict")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(
+        logging.Formatter(
+            fmt="%(asctime)s %(levelname)8s | %(message)s (%(filename)s:%(lineno)d)",
+            datefmt=r"[%m/%d %H:%M:%S]",
+        )
+    )
+    file_handler.addFilter(LoggerFilterGramAddictOnly())
+    return file_handler
 
-    # Console handler (limited colored log)
+
+def configure_logger():
+    global g_session_id
+    global g_log_file_name
+    global g_logs_dir
+    global g_file_handler
+
+    g_session_id = uuid4()
+    g_logs_dir = "logs"
+    g_log_file_name = f"{g_session_id}.log"
+
+    init_colorama()
+
+    # Root logger
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    # Console logger (limited but colored log)
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
-    console_handler.setFormatter(console_formatter)
-    console_handler.addFilter(FilterGramAddictOnly())
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(
+        ColoredFormatter(
+            fmt="%(asctime)s %(levelname)8s | %(message)s", datefmt="[%m/%d %H:%M:%S]"
+        )
+    )
+    console_handler.addFilter(LoggerFilterGramAddictOnly())
+    root_logger.addHandler(console_handler)
 
-    # Crash report handler (full raw log)
-    global log_stream
-    log_stream = StringIO()
-    crash_report_handler = logging.StreamHandler(log_stream)
-    crash_report_handler.setLevel(logging.DEBUG)
-    crash_report_handler.setFormatter(crash_report_formatter)
-    crash_report_handler.addFilter(FilterGramAddictOnly())
+    # File logger (full raw log)
+    if not os.path.exists(g_logs_dir):
+        os.makedirs(g_logs_dir)
+    g_file_handler = create_log_file_handler(f"{g_logs_dir}/{g_log_file_name}")
+    root_logger.addHandler(g_file_handler)
 
-    logger.addHandler(console_handler)
-    logger.addHandler(crash_report_handler)
+    init_logger = logging.getLogger(__name__)
+    init_logger.debug(f"Initial log file: {g_logs_dir}/{g_log_file_name}")
 
 
-def get_logs():
-    # log_stream is a StringIO() created when configure_logger() is called
-    global log_stream
+def get_log_file_config():
+    return g_log_file_name, g_logs_dir, g_file_handler, g_session_id
 
-    return log_stream.getvalue()
+
+def update_log_file_name(username: str):
+    unamed_log_file_name, logs_dir, file_handler, _ = get_log_file_config()
+    unamed_full_filename = f"{logs_dir}/{unamed_log_file_name}"
+
+    init_logger = logging.getLogger(__name__)
+    if not username:
+        init_logger.error(f"No username found, using log file {unamed_full_filename}")
+        return
+    named_log_file_name = f"{username}.log"
+    named_full_filename = f"{logs_dir}/{named_log_file_name}"
+    rollover = False
+    if os.path.isfile(named_full_filename):
+        rollover = True
+
+    named_file_handler = create_log_file_handler(named_full_filename)
+    if rollover:
+        named_file_handler.doRollover()
+
+    # copy existing runtime logs (uidd4.log) to named log file (username.log)
+    with open(unamed_full_filename, "r") as unamed_file, open(
+        named_full_filename, "a"
+    ) as named_file:
+        for line in unamed_file:
+            named_file.write(line)
+
+    root_logger = logging.getLogger()
+    root_logger.removeHandler(file_handler)
+    os.remove(unamed_full_filename)
+    root_logger.addHandler(named_file_handler)
+    root_logger.debug(f"Updated log file: {named_full_filename}")
+
+    global g_log_file_name
+    global g_file_handler
+    g_log_file_name = named_log_file_name
+    g_file_handler = named_file_handler
