f52e085aefe6a0ea9800e6653eeee7c7e64b3e99
Philip Ulrich <philip@ulrich.life>
2020-11-18 20:49:31 -0600

    resolving conflicts

--- GramAddict/__init__.py
+++ GramAddict/__init__.py
@@ -7,7 +7,7 @@ from time import sleep
 from colorama import Fore, Style
 
 from GramAddict.core.device_facade import create_device
-from GramAddict.core.log import configure_logger
+from GramAddict.core.log import configure_logger, update_log_file_name
 from GramAddict.core.navigation import switch_to_english
 from GramAddict.core.persistent_list import PersistentList
 from GramAddict.core.plugin_loader import PluginLoader
@@ -127,20 +127,20 @@ def run():
         )
         return
 
-    session_state = SessionState()
-    session_state.args = args.__dict__
-    sessions.append(session_state)
-
     device_id = args.device
     if not check_adb_connection(is_device_id_provided=(device_id is not None)):
         return
-    logger.info("Instagram version: " + get_instagram_version())
+    logger.info("Instagram version: " + get_instagram_version(device_id))
     device = create_device(device_id)
 
     if device is None:
         return
 
     while True:
+        session_state = SessionState()
+        session_state.args = args.__dict__
+        sessions.append(session_state)
+
         logger.info(
             "-------- START: " + str(session_state.startTime) + " --------",
             extra={"color": f"{Style.BRIGHT}{Fore.YELLOW}"},
@@ -148,6 +148,7 @@ def run():
 
         if args.screen_sleep:
             screen_sleep(device_id, "on")  # Turn on the device screen
+
         open_instagram(device_id)
 
         try:
@@ -178,15 +179,19 @@ def run():
                 "Could not get one of the following from your profile: username, # of followers, # of followings. This is typically due to a soft ban. Review the crash screenshot to see if this is the case."
             )
             logger.critical(
-                f"Username: {getattr(session_state,'my_username')}, Followers: {getattr(session_state,'my_followers_count')}, Following: {getattr(session_state,'my_following_count')}"
+                f"Username: {session_state.my_username}, Followers: {session_state.my_followers_count}, Following: {session_state.my_following_count}"
             )
             save_crash(device)
             exit(1)
 
-        username = session_state.my_username
-        followers = session_state.my_followers_count
-        following = session_state.my_following_count
-        report_string = f"Hello, @{username}! You have {followers} followers and {following} followings so far."
+        try:
+            update_log_file_name(session_state.my_username)
+        except:
+            logger.error("Failed to update log file name. Will continue anyway.")
+            save_crash(device)
+
+        report_string = f"Hello, @{session_state.my_username}! You have {session_state.my_followers_count} followers and {session_state.my_following_count} followings so far."
+
         logger.info(report_string, extra={"color": f"{Style.BRIGHT}"})
 
         storage = Storage(session_state.my_username)
