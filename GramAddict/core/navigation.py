b47813620455ab6e273ec3aa2a207734d9b0b183
ugur-kellecioglu <ugur.kellecioglu@outlook.com>
2020-11-23 18:29:13 +0300

    plugin interact with a user's post likers

--- GramAddict/core/navigation.py
+++ GramAddict/core/navigation.py
@@ -2,6 +2,8 @@ import logging
 
 from colorama import Fore
 from GramAddict.core.views import TabBarView
+from GramAddict.core.utils import  random_sleep
+from GramAddict.core.device_facade import DeviceFacade
 
 logger = logging.getLogger(__name__)
 
@@ -16,3 +18,32 @@ def switch_to_english(device):
     account_view = settingts_view.navigateToAccount()
     language_view = account_view.navigateToLanguage()
     language_view.setLanguage("english")
+
+def open_user(device, username):
+        search_view = TabBarView(device).navigateToSearch()
+        profile_view = search_view.navigateToUsername(username)
+        random_sleep()
+        if not profile_view:
+            return False
+
+        return True
+def open_likers(device):
+        attempts = 0
+        while True:
+            likes_view = device.find(
+                resourceId="com.instagram.android:id/row_feed_textview_likes",
+                className="android.widget.TextView",
+            )
+            if likes_view.exists():
+                logger.info("Opening post likers")
+                random_sleep()
+                likes_view.click("right")
+                return True
+            else:
+                if attempts < 1:
+                    attempts += 1
+                    logger.info("Can't find likers, trying small swipe")
+                    device.swipe(DeviceFacade.Direction.TOP, scale=0.1)
+                    continue
+                else:
+                    return False
--- GramAddict/plugins/interact_post_likers.py
+++ GramAddict/plugins/interact_post_likers.py
@@ -2,7 +2,7 @@
 import logging
 from enum import Enum, unique
 from functools import partial
-from random import seed, shuffle
+from random import randint, seed, shuffle
 from time import sleep
 
 from colorama import Fore
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 
 from GramAddict.core.views import TabBarView
 
-
+from GramAddict.core.navigation import open_user, open_likers
 
 BUTTON_REGEX = "android.widget.Button"
 BUTTON_OR_TEXTVIEW_REGEX = "android.widget.Button|android.widget.TextView"
@@ -154,39 +154,34 @@ class InteractPostLikers(Plugin):
             session_state=self.session_state,
         )
 
-        if not self.open_user(device, username):
+        if not open_user(device, username):
             return
 
-        """can_follow = (
-            not is_myself
-            and not is_follow_limit_reached()
-            and storage.get_following_status(username) == FollowingStatus.NONE
-        )
-
-        interaction_succeed, followed = interaction(
-            device, username=username, can_follow=can_follow
-        )
-        ##storage.add_interacted_user(username, followed=followed)
-        can_continue = on_interaction(succeed=interaction_succeed, followed=followed)"""
+        
         profile_view = ProfileView(device)
         is_private = profile_view.isPrivateAccount()
         posts_count = profile_view.getPostsCount()
         posts_tab_view = profile_view.navigateToPostsTab()
-        likes_value = get_value(likes_count, "Likes count: {}", 2)
         if posts_tab_view.scrollDown():  # scroll down to view all maximum 12 posts
             logger.info("Scrolled down to see more posts.")
         random_sleep()
-        number_of_rows_to_use = min((likes_value * 2) // 3 + 1, 4)
+        number_of_rows_to_use = 4
         photos_indices = list(range(0, number_of_rows_to_use * 3))
         shuffle(photos_indices)
-        photos_indices = photos_indices[:likes_value]
         photos_indices = sorted(photos_indices)
-
-        photo_index = photos_indices[0]
-        row = photo_index // 3
-        column = photo_index - row * 3
-        logger.info(f"Open post #{ 1} ({row + 1} row, {column + 1} column")
-        opened_post_view = posts_tab_view.navigateToPost(row, column)
+        print(photos_indices)
+        x = randint(0,len(photos_indices) - 1)
+        photo_index = photos_indices[x] 
+        opened_post_view = None
+        row = 0
+        column = 0
+        while(opened_post_view == None):
+            x = randint(0,1)
+            photo_index = photos_indices[x]
+            row = photo_index // 3
+            column = photo_index - row * 3
+            opened_post_view = posts_tab_view.navigateToPost(row, column)
+        logger.info(f"Open post  ({row + 1} row, {column + 1} column)")
         random_sleep()
         posts_list_view = device.find(
             resourceId="android:id/list",
@@ -196,7 +191,7 @@ class InteractPostLikers(Plugin):
         posts_end_detector = ScrollEndDetector(repeats_to_end=2)
 
         while True:
-            if not self.open_likers(device):
+            if not open_likers(device):
                 logger.info(
                     "No likes, let's scroll down.", extra={"color": f"{Fore.GREEN}"}
                 )
@@ -291,34 +286,8 @@ class InteractPostLikers(Plugin):
             else:
                 posts_list_view.scroll(DeviceFacade.Direction.BOTTOM)
 
-    def open_user(self, device, username):
-        search_view = TabBarView(device).navigateToSearch()
-        profile_view = search_view.navigateToUsername(username)
-        random_sleep()
-        if not profile_view:
-            return False
+    
 
-        return True
-
-    def open_likers(self, device):
-        attempts = 0
-        while True:
-            likes_view = device.find(
-                resourceId="com.instagram.android:id/row_feed_textview_likes",
-                className="android.widget.TextView",
-            )
-            if likes_view.exists():
-                logger.info("Opening post likers")
-                random_sleep()
-                likes_view.click("right")
-                return True
-            else:
-                if attempts < 1:
-                    attempts += 1
-                    logger.info("Can't find likers, trying small swipe")
-                    device.swipe(DeviceFacade.Direction.TOP, scale=0.1)
-                    continue
-                else:
-                    return False
+    
         
     
