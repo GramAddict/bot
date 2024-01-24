92e3453adc5ed900e6de410f21b7a46ca23176f8
Dennis <mastrolube@gmail.com>
2024-01-24 07:48:27 +0100

    bump: update version

--- GramAddict/__init__.py
+++ GramAddict/__init__.py
@@ -1,5 +1,5 @@
 """Human-like Instagram bot powered by UIAutomator2"""
-__version__ = "3.2.8b0"
+__version__ = "3.2.8"
 __tested_ig_version__ = "263.2.0.19.104"
 
 from GramAddict.core.bot_flow import start_bot
