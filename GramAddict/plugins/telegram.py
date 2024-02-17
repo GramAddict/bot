1639884b3b136cd86f0b9523d9507ce99cfcc516
Dennis <mastrolube@gmail.com>
2024-02-17 18:03:22 +0100

    fix: improved telegram reports

--- GramAddict/plugins/telegram.py
+++ GramAddict/plugins/telegram.py
@@ -1,6 +1,8 @@
 import json
 import logging
 from datetime import datetime
+from typing import Optional
+
 import requests
 import yaml
 from colorama import Fore, Style
@@ -8,6 +10,145 @@ from colorama import Fore, Style
 from GramAddict.core.plugin_loader import Plugin
 
 logger = logging.getLogger(__name__)
+
+
+def load_sessions(username) -> Optional[dict]:
+    try:
+        with open(f"accounts/{username}/sessions.json") as json_data:
+            return json.load(json_data)
+    except FileNotFoundError:
+        logger.error("No session data found. Skipping report generation.")
+        return None
+
+
+def load_telegram_config(username) -> Optional[dict]:
+    try:
+        with open(f"accounts/{username}/telegram.yml", "r", encoding="utf-8") as stream:
+            return yaml.safe_load(stream)
+    except FileNotFoundError as e:
+        logger.error(f"Configuration file not found: {e}")
+        return None
+
+
+def telegram_bot_send_text(bot_api_token, bot_chat_ID, text):
+    try:
+        method = "sendMessage"
+        parse_mode = "markdown"
+        params = {"text": text, "chat_id": bot_chat_ID, "parse_mode": parse_mode}
+        url = f"https://api.telegram.org/bot{bot_api_token}/{method}"
+        return requests.get(url, params=params).json()
+    except Exception as e:
+        logger.error(f"Error sending Telegram message: {e}")
+        return None
+
+
+def _initialize_aggregated_data():
+    return {
+        "total_likes": 0,
+        "total_watched": 0,
+        "total_followed": 0,
+        "total_unfollowed": 0,
+        "total_comments": 0,
+        "total_pm": 0,
+        "duration": 0,
+        "followers": float("inf"),
+        "following": float("inf"),
+        "followers_gained": 0,
+    }
+
+
+def _calculate_session_duration(session):
+    try:
+        start_datetime = datetime.strptime(
+            session["start_time"], "%Y-%m-%d %H:%M:%S.%f"
+        )
+        finish_datetime = datetime.strptime(
+            session["finish_time"], "%Y-%m-%d %H:%M:%S.%f"
+        )
+        return int((finish_datetime - start_datetime).total_seconds() / 60)
+    except ValueError:
+        logger.debug(
+            f"{session['id']} has no finish_time. Skipping duration calculation."
+        )
+        return 0
+
+
+def daily_summary(sessions):
+    daily_aggregated_data = {}
+    for session in sessions:
+        date = session["start_time"][:10]
+        daily_aggregated_data.setdefault(date, _initialize_aggregated_data())
+        duration = _calculate_session_duration(session)
+        daily_aggregated_data[date]["duration"] += duration
+
+        for key in [
+            "total_likes",
+            "total_watched",
+            "total_followed",
+            "total_unfollowed",
+            "total_comments",
+            "total_pm",
+        ]:
+            daily_aggregated_data[date][key] += session.get(key, 0)
+
+        daily_aggregated_data[date]["followers"] = min(
+            session.get("profile", {}).get("followers", 0),
+            daily_aggregated_data[date]["followers"],
+        )
+        daily_aggregated_data[date]["following"] = min(
+            session.get("profile", {}).get("following", 0),
+            daily_aggregated_data[date]["following"],
+        )
+    return _calculate_followers_gained(daily_aggregated_data)
+
+
+def _calculate_followers_gained(aggregated_data):
+    dates_sorted = list(sorted(aggregated_data.keys()))[-2:]
+    previous_followers = None
+    for date in dates_sorted:
+        current_followers = aggregated_data[date]["followers"]
+        if previous_followers is not None:
+            aggregated_data[date]["followers_gained"] = (
+                current_followers - previous_followers
+            )
+        previous_followers = current_followers
+    return aggregated_data
+
+
+def generate_report(
+    username, last_session, daily_aggregated_data, followers_now, following_now
+):
+    return f"""
+            *Stats for {username}*:
+
+            *✨Overview after last activity*
+            • {followers_now} followers ({followers_now - last_session.get("profile", {}).get("followers", 0):+})
+            • {following_now} following ({following_now - last_session.get("profile", {}).get("following", 0):+})
+
+            *🤖 Last session actions*
+            • {last_session["duration"]} minutes of botting
+            • {last_session["total_likes"]} likes
+            • {last_session["total_followed"]} follows
+            • {last_session["total_unfollowed"]} unfollows
+            • {last_session["total_watched"]} stories watched
+            • {last_session["total_comments"]} comments done
+            • {last_session["total_pm"]} PM sent
+
+            *📅 Today's total actions*
+            • {daily_aggregated_data["duration"]} minutes of botting
+            • {daily_aggregated_data["total_likes"]} likes
+            • {daily_aggregated_data["total_followed"]} follows
+            • {daily_aggregated_data["total_unfollowed"]} unfollows
+            • {daily_aggregated_data["total_watched"]} stories watched
+            • {daily_aggregated_data["total_comments"]} comments done
+            • {daily_aggregated_data["total_pm"]} PM sent
+
+            *📈 Trends*
+            • {daily_aggregated_data["followers_gained"]} new followers today
+
+            *🗓 7-Day Average*
+            TODO
+        """
 
 
 class TelegramReports(Plugin):
@@ -31,105 +172,31 @@ class TelegramReports(Plugin):
             logger.error("You have to specify a username for getting reports!")
             return
 
-        def telegram_bot_sendtext(text):
-            try:
-                with open(
-                    f"accounts/{username}/telegram.yml", "r", encoding="utf-8"
-                ) as stream:
-                    config = yaml.safe_load(stream)
-                bot_api_token = config.get("telegram-api-token")
-                bot_chat_ID = config.get("telegram-chat-id")
-                if bot_api_token and bot_chat_ID:
-                    method = "sendMessage"
-                    parse_mode = "markdown"
-                    params = {
-                        "text": text,
-                        "chat_id": bot_chat_ID,
-                        "parse_mode": parse_mode,
-                    }
-                    url = f"https://api.telegram.org/bot{bot_api_token}/{method}"
-                    response = requests.get(url, params=params)
-                    return response.json()
-            except Exception as e:
-                logger.error(f"Error sending Telegram message: {e}")
-                return None
-
-        try:
-            with open(f"accounts/{username}/sessions.json") as json_data:
-                sessions = json.load(json_data)
-        except FileNotFoundError:
-            logger.error("No session data found. Skipping report generation.")
+        sessions = load_sessions(username)
+        if not sessions:
+            logger.error(
+                f"No session data found for {username}. Skipping report generation."
+            )
             return
 
-        aggregated_data = {}
+        last_session = sessions[-1]
+        last_session["duration"] = _calculate_session_duration(last_session)
 
-        for session in sessions:
-            date = session["start_time"][:10]
-            if date not in aggregated_data:
-                aggregated_data[date] = {
-                    "total_likes": 0,
-                    "total_watched": 0,
-                    "total_followed": 0,
-                    "total_unfollowed": 0,
-                    "total_comments": 0,
-                    "total_pm": 0,
-                    "duration": 0,
-                    "followers": 0,
-                }
-            try:
-                start_datetime = datetime.strptime(
-                    session["start_time"], "%Y-%m-%d %H:%M:%S.%f"
-                )
-                finish_datetime = datetime.strptime(
-                    session["finish_time"], "%Y-%m-%d %H:%M:%S.%f"
-                )
-                duration = int((finish_datetime - start_datetime).total_seconds() / 60)
-            except ValueError:
-                logger.error(f"Failed to calculate session duration for {date}.")
-                duration = 0
-            aggregated_data[date]["duration"] += duration
-
-            for key in [
-                "total_likes",
-                "total_watched",
-                "total_followed",
-                "total_unfollowed",
-                "total_comments",
-                "total_pm",
-            ]:
-                aggregated_data[date][key] += session.get(key, 0)
-            aggregated_data[date]["followers"] = session.get("profile", {}).get(
-                "followers", 0
+        telegram_config = load_telegram_config(username)
+        if not telegram_config:
+            logger.error(
+                f"No telegram configuration found for {username}. Skipping report generation."
             )
+            return
 
-        # Calculate followers gained
-        dates_sorted = list(sorted(aggregated_data.keys()))[-2:]
-        previous_followers = None
-        for date in dates_sorted:
-            current_followers = aggregated_data[date]["followers"]
-            if previous_followers is not None:
-                aggregated_data[date]["followers_gained"] = (
-                    current_followers - previous_followers
-                )
-            else:
-                aggregated_data[date][
-                    "followers_gained"
-                ] = 0  # No data to compare for the first entry
-            previous_followers = current_followers
-
-        # TODO: Add more stats
-        report = f"Stats for {username}:\n\n"
-        for date, data in list(aggregated_data.items())[-2:]:
-            report += f"Date: {date}\n"
-            report += f"Duration (min): {data['duration']:.2f}\n"
-            report += f"Likes: {data['total_likes']}, Watched: {data['total_watched']}, Followed: {data['total_followed']}, Unfollowed: {data['total_unfollowed']}\n"
-            report += (
-                f"Comments: {data['total_comments']}, PM Sent: {data['total_pm']}\n"
-            )
-            report += f"Followers Gained: {data['followers_gained']}\n\n"
-
-        # Send the report via Telegram
-        response = telegram_bot_sendtext(report)
+        daily_aggregated_data = daily_summary(sessions)
+        today_data = daily_aggregated_data.get(last_session["start_time"][:10], {})
+        report = generate_report(
+            username, last_session, today_data, followers_now, following_now
+        )
+        response = telegram_bot_send_text(
+            report, telegram_config["chat_id"], telegram_config["api_token"]
+        )
         if response and response.get("ok"):
             logger.info(
                 "Telegram message sent successfully.",
