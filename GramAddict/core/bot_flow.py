f9060099110a76b52a82c2ec6a3538f60ee8c0a3
Dennis <mastrolube@gmail.com>
2024-03-17 14:32:13 +0100

    fix: typo

--- GramAddict/core/bot_flow.py
+++ GramAddict/core/bot_flow.py
@@ -166,7 +166,7 @@ def start_bot(**kwargs):
                             "If you press ENTER, you are aware of this and will not ask for support in case of a crash."
                         )
                         logger.warning(
-                            "If you want to avoid pressing ENTER next run, add allow-untested-ig-version: True in your config.yml file. (read the docs for more info)"
+                            "If you want to avoid pressing ENTER next run, add allow-untested-ig-version: true in your config.yml file. (read the docs for more info)"
                         )
                         input()
 
